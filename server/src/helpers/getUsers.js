b4fba2d93898bdb6e79daafcbdbfe746aabc9fcf
Jose Eduardo R Manso <manso@manso.com.br>
2019-03-22 15:29:44 -0300

    some server log messages

--- server/src/helpers/getUsers.js
+++ server/src/helpers/getUsers.js
@@ -1,40 +1,39 @@
 import { users } from '../model';
 
 export default function getUsers(filter, { first, after }) {
+  console.log(`query: users(first: ${first}, after: ${after}}`);
 
-  const filtered = users.filter(filter)
+  const filtered = users.filter(filter);
   const totalCount = filtered.length;
 
   after = users.ensureKey(after);
-  let limit = (Number.isInteger(first) && first > 0)
-    ? first
-    : totalCount
+  let limit = Number.isInteger(first) && first > 0 ? first : totalCount;
 
-  let start
+  let start;
   if (after) {
     for (let i = 0; i < filtered.length; i++) {
-      const item = filtered[i] // item => [key, value]
+      const item = filtered[i]; // item => [key, value]
       if (item[0] === after) {
         start = i + 1;
         break;
       }
     }
   } else {
-    start = 0
+    start = 0;
   }
-  let hasNextPage = limit && limit < (filtered.length - start)
+  let hasNextPage = limit && limit < filtered.length - start;
 
-  const edges = filtered
-    .slice(start, start + limit)
-    .map(item => {
-      return {
-        cursor: item[0],
-        node: { ...item[1] }
-      }
-    })
+  const edges = filtered.slice(start, start + limit).map(item => {
+    return {
+      cursor: item[0],
+      node: { ...item[1] }
+    };
+  });
 
-  let endCursor = edges[edges.length -1]
-  if (endCursor) { endCursor = endCursor.cursor }
+  let endCursor = edges[edges.length - 1];
+  if (endCursor) {
+    endCursor = endCursor.cursor;
+  }
 
   return {
     count: edges.length,
@@ -44,5 +43,5 @@ export default function getUsers(filter, { first, after }) {
       hasNextPage
     },
     edges
-  }
+  };
 }
